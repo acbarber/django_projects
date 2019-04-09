6cf2b6d51dea883ae0cdfe010c599abfdc1882aa
Andrew Barber <acbarber@umich.edu>
2019-04-09 22:03:30 +0000

    A lot of stuff

---
+++ adlist/ads/views.py
@@ -0,0 +1,88 @@
+from ads.models import Ad
+
+from django.views import View
+from django.views import generic
+from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse_lazy
+from django.http import HttpResponse
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+from django.core.files.uploadedfile import InMemoryUploadedFile
+
+from ads.util import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
+
+from ads.forms import CreateForm
+
+class AdListView(OwnerListView):
+    model = Ad
+    template_name = "ad_list.html"
+
+class AdDetailView(OwnerDetailView):
+    model = Ad
+    template_name = "ad_detail.html"
+
+# class AdCreateView(OwnerCreateView):
+#     model = Ad
+#     fields = ['title', 'text', 'price']
+#     template_name = "ad_form.html"
+
+# class AdUpdateView(OwnerUpdateView):
+#     model = Ad
+#     fields = ['title', 'text']
+#     template_name = "ad_form.html"
+
+class AdDeleteView(OwnerDeleteView):
+    model = Ad
+    template_name = "ad_delete.html"
+
+class AdCreateView(LoginRequiredMixin, View):
+    template = 'ad_form.html'
+    success_url = reverse_lazy('ads')
+    def get(self, request, pk=None) :
+        form = CreateForm()
+        ctx = { 'form': form }
+        return render(request, self.template, ctx)
+
+    def post(self, request, pk=None) :
+        form = CreateForm(request.POST, request.FILES or None)
+
+        if not form.is_valid() :
+            ctx = {'form' : form}
+            return render(request, self.template, ctx)
+
+        # Add owner to the model before saving
+        pic = form.save(commit=False)
+        pic.owner = self.request.user
+        pic.save()
+        return redirect(self.success_url)
+
+class AdUpdateView(LoginRequiredMixin, View):
+    template = 'ad_form.html'
+    success_url = reverse_lazy('ads')
+    def get(self, request, pk) :
+        pic = get_object_or_404(Ad, id=pk, owner=self.request.user)
+        form = CreateForm(instance=pic)
+        ctx = { 'form': form }
+        return render(request, self.template, ctx)
+
+    def post(self, request, pk=None) :
+        pic = get_object_or_404(Ad, id=pk, owner=self.request.user)
+        form = CreateForm(request.POST, request.FILES or None, instance=pic)
+
+        if not form.is_valid() :
+            ctx = {'form' : form}
+            return render(request, self.template, ctx)
+
+        pic = form.save(commit=False)
+        pic.owner = self.request.user
+        pic.save()
+        return redirect(self.success_url)
+
+def stream_file(request, pk) :
+    ad = get_object_or_404(Ad, id=pk)
+    response = HttpResponse()
+    response['Content-Type'] = ad.content_type
+    response['Content-Length'] = len(ad.picture)
+    response.write(ad.picture)
+    return response
